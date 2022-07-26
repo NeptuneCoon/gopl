4facef4d7955494fd175f31f1b10cbc3c17a5c14
gao <mercury337@outlook.com>
2022-07-26 23:42:48 +0800

    ch3

---
+++ ch3/basename1.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// 获取文件名
+func basename(s string) string {
+	// 将最后一个'/'之前的部分全部舍弃
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '/' {
+			s = s[i+1:]
+			break
+		}
+	}
+	// 保留最后一个'.'之前的所有内容
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == '.' {
+			s = s[:i]
+			break
+		}
+	}
+	return s
+}
+
+func main() {
+	s := "C:/temp/hello.txt"
+	fmt.Printf(basename(s))
+}
